d61ea93ade6d543ccb68274f58c5a172d19836db
thienhuu163 <thienhuu1603@gmail.com>
2023-11-23 22:00:10 +0700

    da xong thongke

--- admin/thongke.php
+++ admin/thongke.php
@@ -17,50 +17,35 @@
 
         <div class="content" >
 
-            <div align="center"><h2 style="font-size:25px;">Thống Kê</h2></div>
-            <td>
-                <tr align="left">Từ ngày:<input style="font-size:18px;" type="text" id="startDate" ></tr>
-</td>
-<td>
-                <tr align="right">Đến ngày:<input style="font-size:18px;" type="text" id="endDate"></tr>
-            </td>
-            <table class="customer-table" style="border: 4px solid #dddddd;">
-            <script>
-                    $(function() 
-                    {
-                    $("#startDate").datepicker(
-                    {
-                     dateFormat: "dd/mm/yy",
-                        onSelect: function(selectedDate) 
-                        {
-                        $("#endDate").datepicker("option", "minDate", selectedDate);
-                    }
-                    });
-                        $("#endDate").datepicker(
-                    {
-                     dateFormat: "dd/mm/yy",
-                        onSelect: function(selectedDate) 
-                    {
-                        $("#startDate").datepicker("option", "maxDate", selectedDate);
-                    }
-                    });
-                    });
+            <div align="center"><h2 style="font-size:40px;">Thống Kê</h2></div>
+            
+            <label for="picker1">Từ ngày:</label>
+<input type="date" id="picker1" name="picker1">
+
+<label for="picker2">Đến ngày:</label>
+<input type="date" id="picker2" name="picker2">
+            
 
             </script>
-                <div class="search-container" style="text-align:center; margin-top:8px; border-radius: 20px;" >
-                <input style="font-size:18px; " type="text" id="searchInput" class="search-box" placeholder="Tìm kiếm...">
-                <button style="font-size:18px;" onclick="performSearch()" class="search-btn">Mã sản phẩm</button>
-                </div>
+            <div class="search-container" style="text-align:left; margin-top:10px; " >
+                <input style="font-size:14px; border-radius: 40px;" 
+                    type="text" id="searchInput" class="search-box" placeholder="Tìm kiếm...">
+                <button style="font-size:14px;" 
+                    onclick="performSearch()" class="search-btn">Mã sản phẩm</button>
+            </div>
 
-            <table class="customer-table">
+            <table class="customer-table" style="border: 4px solid #dddddd;">
                 <thead>
                     <tr>
-                        <th>Mã sách</th>
+                        <th>Mã sách</th>   
                         <th>Tên sách</th>
                         <th>Số lượng</th>
                         <th>Giá</th>
                         <th>Thành tiền</th>
-                        <th>Tổng</th>
+                        </tr>
+                        <tr>
+                        <td style=" text-align:left;" colspan="4">Tổng</td>   
+                        <th colspan="2"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -75,7 +60,7 @@
                             <td> <?= $row['tensach'] ?></td>
                             <td> <?= $row['soluong']?> </td>
                             <td><?= $row['gia'] ?></td>
-                            <td> <?= $row['thanhtien']?> 
+                            <td> <?= $row['thanhtien']?>    
                             <td> <?= $row['tong']?> <
                             <td><a href="http://localhost/bansach/THLVN/admin/thongke.php?id=<?$row['id']?>" >Sửa</a></td>
                             </tr>
